0a996d72fb1f95cbf4dcfaa4347638220525a35d
luisegarduno <luis.e.garduno@gmail.com>
2020-11-14 20:36:25 -0600

    - Docker-compose up WORKS, - README.md Instructions updated

--- README.md
+++ README.md
@@ -8,20 +8,10 @@ folder & enter cloud db credentials :
 > MYSQL_CLOUD_USER=<br>
   MYSQL_CLOUD_PASS=<br>
   MYSQL_CLOUD_HOST=
-
-
-**2.)** Install node_modules (if not already installed)
-
-- Backend  &rarr; `$ cd FrontAndBackEnd/backend; yarn`
-- Frontend &rarr; `$ cd FrontAndBackEnd/frontend; yarn`
-
-Or Both Simultaneously &rarr; `$ cd FrontAndBackEnd/backend; yarn; cd ../frontend; yarn`
-
------------------------- 
+ 
 
 ## How To Deploy
+- Frontend & Backend (Using Docker) &rarr; `$ docker-compose up`
 - Backend ONLY &rarr; `$ cd FrontAndBackEnd/backend; yarn run dev`
 - Frontend ONLY &rarr; `$ cd FrontAndBackEnd/frontend; yarn start`
-- Frontend & Backend *(Not working properly)* &rarr; `$ docker-compose build; docker-compose up`
-
 
--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,17 @@ const cors = require('cors');
 const mysql = require('mysql');
 const { log, ExpressAPILogMiddleware } = require('@rama41222/node-logger');
 
-// mysql connection
-var connection = mysql.createConnection({
+// *****************************************************
+
+var db_config = {
   host: process.env.MYSQL_CLOUD_HOST,
   user: process.env.MYSQL_CLOUD_USER,
   password: process.env.MYSQL_CLOUD_PASS,
   port: process.env.MYSQL_PORT,
   database: process.env.MYSQL_DB
-});
+};
+
+var connection;
 
 // set up some configs for express.
 const config = {
@@ -33,12 +36,35 @@ app.use(cors({
 }));
 app.use(ExpressAPILogMiddleware(logger, { request: true }));
 
-// Attempting to connect to the database.
-connection.connect(function (err) {
-  if (err)
-    logger.error("Cannot connect to DB!");
-  logger.info("Connected to the DB!");
-});
+
+// *****************************************************
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since the old one cannot be reused
+
+  connection.connect(function(err) {              // The server is either down or restarting (takes a while sometimes).
+    if(err) {
+      logger.error("Cannot connect to DB!");
+      console.log('Error when connecting to DB:', err);
+      setTimeout(handleDisconnect, 2000); // Introduce a delay before attempting to reconnect, to avoid a hot loop, & to allow node script to 
+    }
+    else{
+      logger.info("Connected to the DB!");
+    }
+  });
+
+  connection.on('error', function(err) {
+    console.log('DB error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually lost due to either server restart or
+      handleDisconnect();                         // a connection idle timeout (the wait_timeout server variable configures this)
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
+// *****************************************************
+
 
 // GET /
 app.get('/', (req, res) => {
