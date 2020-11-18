250f1c468f1385858221ab038bd07437823d1b5d
Nathan Wesley <nathanswesley@gmail.com>
2020-11-17 23:29:27 -0600

    account validation

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,25 @@
 const router = require('express').Router()
+const logger = require('@rama41222/node-logger/src/logger')
 const connection = require('../connection')
+
+router.post('/', async (req, res) => {
+  var username = req.param('username')
+  var password = req.param('password')     // Set up table to have an auto increment primary key
+  connection.query("INSERT INTO user (username, password) VALUES(?, ?)", [username, password], function (err, rows, fields) {
+    if (err) {
+      logger.error("Error while executing Query: \n", err)
+      res.status(400).json({
+        "data": [],
+        "error": "MySQL error"
+      })
+    }
+    else{
+      res.status(200).json({
+        "data": rows
+      })
+    }
+  })
+})
 
 router.get('/', (req, res) => {
   connection.query('SELECT * FROM user', function (err, rows, fields) {
@@ -36,10 +56,20 @@ router.get('/:username', function (req, res) {
   })
 })
 
-router.post('/', async (req, res) => {
-  var username = req.param('username')
-  var password = req.param('password')     // Set up table to have an auto increment primary key
-  connection.query("INSERT INTO user (username, password) VALUES(?, ?)", [username, password], function (err, rows, fields) {
+router.put('/:username', async (req, res) => {
+  let password = req.body.password
+  let username = req.params.username
+  connection.query('UPDATE user SET password = ? WHERE username = ?', [password, username], function (err, result, fields) {
+    if (err) throw err
+    res.end(JSON.stringify(result))      // Result in JSON format
+    });
+});
+
+router.post('/login', async (req, res) => {
+  const username = req.body.username
+  const password = req.body.password
+
+  connection.query("SELECT COUNT(*) AS users_count FROM user WHERE username = ? AND PASSWORD = ?", [username, password], (err, rows) => {
     if (err) {
       logger.error("Error while executing Query: \n", err)
       res.status(400).json({
@@ -48,20 +78,10 @@ router.post('/', async (req, res) => {
       })
     }
     else{
-      res.status(200).json({
-        "data": rows
-      })
+      const accountExists = rows[0]['users_count'] > 0
+      res.status(200).json(accountExists)
     }
   })
 })
-
-router.put('/:username', async (req, res) => {
-  var password = req.body.password
-  var username = req.params.username
-  connection.query('UPDATE user SET password = ? WHERE username = ?', [password, username], function (err, result, fields) {
-    if (err) throw err
-    res.end(JSON.stringify(result))      // Result in JSON format
-    });
-});
 
 module.exports = router
