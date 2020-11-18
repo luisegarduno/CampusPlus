43d04809f952d1f3f32b407a41a761b30c270416
gracedillingham <44414278+gracedillingham@users.noreply.github.com>
2020-11-18 04:46:15 -0600

    AddingPages

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,66 +3,63 @@ const logger = require('@rama41222/node-logger/src/logger')
 const connection = require('../connection')
 
 router.get('/', (req, res) => {
-  const username = req.query.username
+    const username = req.query.username
 
-  connection.query("SELECT * FROM user WHERE username = ?", [username], (err, rows) => {
-    if (err) {
-      logger.error("Error while executing Query: \n", err);
-      res.status(400).json({
-        "data": [],
-        "error": "MySQL error"
-      })
-    }
-    else{
-      res.status(200).json(rows)
-    }
-  })
+    connection.query("SELECT * FROM user WHERE username = ?", [username], (err, rows) => {
+        if (err) {
+            logger.error("Error while executing Query: \n", err);
+            res.status(400).json({
+                "data": [],
+                "error": "MySQL error"
+            })
+        } else {
+            res.status(200).json(rows)
+        }
+    })
 })
 
-router.put('/', async (req, res) => {
-  let password = req.body.password
-  let username = req.body.username
-  connection.query('UPDATE user SET password = ? WHERE username = ?', [password, username], function (err, result, fields) {
-    if (err) throw err
-    res.end(JSON.stringify(result))
+router.put('/', async(req, res) => {
+    let password = req.body.password
+    let username = req.body.username
+    connection.query('UPDATE user SET password = ? WHERE username = ?', [password, username], function(err, result, fields) {
+        if (err) throw err
+        res.end(JSON.stringify(result))
     })
 });
 
-router.post('/create', async (req, res) => {
-  const username = req.body.username
-  const password = req.body.password
+router.post('/create', async(req, res) => {
+    const username = req.body.username
+    const password = req.body.password
 
-  connection.query("INSERT INTO user (username, password) VALUES(?, ?)", [username, password], (err) => {
-    if (err) {
-      logger.error("Error while executing Query: \n", err)
-      res.status(400).json({
-        "data": [],
-        "error": "MySQL error"
-      })
-    }
-    else{
-      res.status(200).json("Account Created")
-    }
-  })
+    connection.query("INSERT INTO user (username, password) VALUES(?, ?)", [username, password], (err) => {
+        if (err) {
+            logger.error("Error while executing Query: \n", err)
+            res.status(400).json({
+                "data": [],
+                "error": "MySQL error"
+            })
+        } else {
+            res.status(200).json("Account Created")
+        }
+    })
 })
 
-router.post('/login', async (req, res) => {
-  const username = req.body.username
-  const password = req.body.password
+router.post('/login', async(req, res) => {
+    const username = req.body.username
+    const password = req.body.password
 
-  connection.query("SELECT COUNT(*) AS users_count FROM user WHERE username = ? AND PASSWORD = ?", [username, password], (err, rows) => {
-    if (err) {
-      logger.error("Error while executing Query: \n", err)
-      res.status(400).json({
-        "data": [],
-        "error": "MySQL error"
-      })
-    }
-    else{
-      const accountExists = rows[0]['users_count'] > 0
-      res.status(200).json(accountExists)
-    }
-  })
+    connection.query("SELECT COUNT(*) AS users_count FROM user WHERE username = ? AND PASSWORD = ?", [username, password], (err, rows) => {
+        if (err) {
+            logger.error("Error while executing Query: \n", err)
+            res.status(400).json({
+                "data": [],
+                "error": "MySQL error"
+            })
+        } else {
+            const accountExists = rows[0]['users_count'] > 0
+            res.status(200).json(accountExists)
+        }
+    })
 })
 
 module.exports = router
