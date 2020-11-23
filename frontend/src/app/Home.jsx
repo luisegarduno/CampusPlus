73552e57f9ac1cd3d305966a9f374b10394a9fea
gracedillingham <44414278+gracedillingham@users.noreply.github.com>
2020-11-23 00:51:05 -0600

    Made home page spacing better

--- frontend/src/app/Home.jsx
+++ frontend/src/app/Home.jsx
@@ -8,38 +8,40 @@ export class Home extends React.Component {
     
      render() {
          return(<>
-          <div className ="header">
+        <div className ="header">
             <h1 className = "text-left bg-dark text-white">CampusPlus
                 <button className="btn btn-primary btn-sm float-right rounded" onClick={() => this.props.history.push("/welcomepages")}>Log Out</button>
             </h1> 
         </div>
-         <div className = "p-3 container-sm">
-            <h2 className = " p-2 text-center bg-dark text-white">Welcome, Name!</h2>
-            <div className = " p-3 card-deck row-cols-2">
+         <div className = "container-sm w-50">
+             <header className = "p-3">
+                 <h2 className = " p-2 text-center bg-dark text-white">Welcome, Name!</h2>
+             </header>
+            <div className = " p-3 card-deck row-cols-2 row align-items-center">
                 <div className="card">
                     <img src="/profileIcon.png" className="card-img-top" alt="..."/>
                         <button className="btn btn-primary text-white btn-sm rounded" onClick={() => this.props.history.push("/profiles")}>Edit Profile</button>
                 </div>
                 <div className="card">
-                    <img src="/calendar2.jpg" className="card-img-top" alt="..."/>
+                    <img src="/rightCalendar.png" className="p-3 card-img-top" alt="..."/>
                         <button className="btn btn-primary text-white btn-sm rounded" onClick={() => this.props.history.push("/calendars")}>View Calendar</button>
                 </div>
                 <div className="card">
-                    <img src="/assignments.png" className="card-img-top" alt="..."/>
+                    <img src="/assignments.png" className="p-3 card-img-top" alt="..."/>
                         <button className="btn btn-primary text-white btn-sm rounded" onClick={() => this.props.history.push("/assignments")}>Add Assignments</button>
                 </div>
              </div>
-            <div className = " p-3 card-deck row-cols-2">
+            <div className = " p-3 card-deck row-cols-2 row align-items-center">
                 <div className="card">
-                    <img src="/archive.png" className="card-img-top" alt="..."/>
+                    <img src="/archive.png" className="p-2 card-img-top" alt="..."/>
                         <button className="btn btn-primary text-white btn-sm rounded" onClick={() => this.props.history.push("/profiles")}>Archived Assignments</button>
                 </div>
                 <div className="card">
-                    <img src="/schedule.png" className="card-img-top" alt="..."/>
-                        <button className="btn btn-primary text-white btn-sm rounded" onClick={() => this.props.history.push("/courses")}>View Schedule And Courses</button>
+                    <img src="/schedule.png" className="p-3 card-img-top" alt="..."/>
+                        <button className="btn btn-primary text-white btn-sm rounded" onClick={() => this.props.history.push("/courses")}>View Courses</button>
                 </div>
                 <div className="card">
-                    <img src="/forum.png" className="card-img-top" alt="..."/>
+                    <img src="/forum.png" className="p-3 card-img-top" alt="..."/>
                         <button className="btn btn-primary text-white btn-sm rounded" onClick={() => this.props.history.push("/homepages")}>Chat Forum</button>
                 </div>
              </div>
