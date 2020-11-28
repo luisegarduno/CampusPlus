035589a075d2c767a6728593e5eb1373c092a32d
gracedillingham <44414278+gracedillingham@users.noreply.github.com>
2020-11-28 00:27:10 -0600

    Update Header

--- frontend/src/app/AssignmentEditor.jsx
+++ frontend/src/app/AssignmentEditor.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 //import { Redirect } from 'react-router-dom'
 //import { ProductRepository } from '../../Api/productRepository'
+import { Header } from './Header';
 
 export class AssignmentEditor extends React.Component{
   
@@ -12,12 +13,11 @@ export class AssignmentEditor extends React.Component{
 
     render() {
         return(<>
-            <div className ="header">
-                <h1 className = "text-left bg-dark text-white">CampusPlus
-                    <button className="btn btn-primary btn-sm float-right rounded" onClick={() => this.props.history.push("/welcomepages")}>Log Out</button>
-                </h1>
-            </div>
-            <div className="p-5 container-sm">
+            <Header />
+            <nav className="navbar bg-white">
+                <span className="mb-0 h5 text-primary">Edit Assignment</span>
+            </nav>
+            <div className="p-5 container-fluid container-sm">
                 <div className = "p-2 card text-center w-50 mx-auto">
                     <div className = "form-group">
                     <div className= "card-header bg-dark text-white">
