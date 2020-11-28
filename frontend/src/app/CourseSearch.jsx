795873b2babba088a602c52ad4d1460d14973c0e
gracedillingham <44414278+gracedillingham@users.noreply.github.com>
2020-11-28 00:50:58 -0600

    Update Header

--- frontend/src/app/CourseSearch.jsx
+++ frontend/src/app/CourseSearch.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import { Header } from './Header';
+
 export class CourseSearch extends React.Component{
 
     render() {
         return(<>
-            <div className ="header">
-                <h1 className = "text-left bg-dark text-white">CampusPlus
-                    <button className="btn btn-primary btn-sm float-right rounded" onClick={() => this.props.history.push("/welcomepages")}>Log Out</button>
-                </h1>
-         </div>
-            <div className = "p-3 container-sm text-center w-50">
+            <Header />
+            <nav className="navbar bg-white">
+                <span className="mb-0 h5 text-primary">Course Search</span>
+            </nav>
+            <div className = "p-3 container-sm container-fluid text-center w-50">
                 <div className ="header"> 
                 </div>
                 <form>
