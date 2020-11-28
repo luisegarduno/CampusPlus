7c0e1c4d3bcecd6189ccbe7040fc0dd24f4d4703
gracedillingham <44414278+gracedillingham@users.noreply.github.com>
2020-11-28 00:58:15 -0600

    Update Header

--- frontend/src/app/Profile.jsx
+++ frontend/src/app/Profile.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './Profile.css';
+import { Header } from './Header';
+
 export class Profile extends React.Component {
 
     render(){
         return(<>
-            <div className ="header">
-                <h1 className = "text-left bg-dark text-white">CampusPlus
-                    <button className="btn btn-primary btn-sm float-right rounded" onClick={() => this.props.history.push("/welcomepages")}>Log Out</button>
-                </h1>
-            </div>
+            <Header />
+            <nav className="navbar bg-white">
+                <span className="mb-0 h5 text-primary">Edit Profile</span>
+            </nav>
             <div className = "p-5 container">
                 <div className = "p-3 jumbotron bg-white text-center">
                  <form>
