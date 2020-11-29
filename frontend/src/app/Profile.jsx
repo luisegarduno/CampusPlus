b296e54d63598e41a4e6d99d50b5960e3e48764a
gracedillingham <44414278+gracedillingham@users.noreply.github.com>
2020-11-28 21:05:31 -0600

    Updated Profile Page

--- frontend/src/app/Profile.jsx
+++ frontend/src/app/Profile.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Profile.css';
+//import './Profile.css';
 import { Header } from './Header';
 
 export class Profile extends React.Component {
@@ -10,50 +10,52 @@ export class Profile extends React.Component {
             <nav className="navbar bg-white">
                 <span className="mb-0 h5 text-primary">Edit Profile</span>
             </nav>
-            <div className = "p-5 container">
-                <div className = "p-3 jumbotron bg-white text-center">
-                 <form>
-                    <div class="form-row">
-                        <div class="form-group col-md-6">
-                            <label for="inputEmail4">Username</label>
-                            <input type="email" class="form-control" id="inputEmail4"/>
-                        </div>
-                        <div class="form-group col-md-6">
-                            <label for="inputPassword4">Password</label>
-                            <input type="password" class="form-control" id="inputPassword4"/>
-                        </div>
+            <div className = "p-3 container-fluid container-sm">
+                <div class="card mb-1 w-75 mx-auto">
+                    <div className = "p-1 jumbotron jumbotron-fluid bg-light text-center">
+                        <img src="/profileIcon.png" className=" img-fluid w-25 rounded mx-auto d-block" alt="..."/>
+                        <button type="submit" className="btn btn-sm btn-primary">Change Image</button>
                     </div>
-                    <div class="form-group">
-                        <label for="inputAddress">Address</label>
-                        <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St"/>
-                    </div>
-                    <div class="form-group">
-                        <label for="inputAddress2">Address 2</label>
-                        <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"/>
-                    </div>
-                    <div class="form-row">
-                        <div class="form-group col-md-6">
-                            <label for="inputCity">City</label>
-                            <input type="text" class="form-control" id="inputCity"/>
+                    <form className = "p-3">
+                        <div className="form-group">
+                            <label htmlFor="username">Username</label>
+                            <input type="text" className="form-control" id="username" placeholder=""/>
                         </div>
-                        <div class="form-group col-md-4">
-                            <label for="inputState">State</label>
-                            <select id="inputState" class="form-control">
-                                <option selected>Choose...</option>
-                                <option>...</option>
-                            </select>
+                        <div className="form-group">
+                            <label htmlFor="email">Email</label>
+                            <input type="text" className="form-control" id="email" placeholder=""/>
                         </div>
-                        <div class="form-group col-md-2">
-                            <label for="inputZip">Zip</label>
-                            <input type="text" class="form-control" id="inputZip"/>
+                        <div className="form-row">
+                            <div className="form-group col-md-6">
+                                <label htmlFor="password">Password</label>
+                                <input type="email" className="form-control" id="password"/>
+                            </div>
+                            <div className="form-group col-md-6">
+                                <label htmlFor="confirmPassword">Password Confirmation</label>
+                                <input type="password" className="form-control" id="confirmPassword"/>
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Save</button>
-                         <p>OR</p>
-                    <button className="btn btn-primary rounded" onClick={() => this.props.history.push("/homepages")}>Return To Home Page</button>
-                </form>
+                        <div className="form-row">
+                            <div className="form-group col-md-6">
+                                <label htmlFor="inputSchool">School</label>
+                                <input type="text" className="form-control" id="inputSchool"/>
+                            </div>
+                            <div className="form-group col-md-4">
+                                <label htmlFor="inputMajor">Major</label>
+                                <input type="text" className="form-control" id="inputMajor"/>
+                            </div>
+                            <div className="form-group col-md-2">
+                                <label htmlFor="gradYear">Grad Year</label>
+                                <select id="gradYear" className="form-control">
+                                    <option selected>Choose...</option>
+                                    <option>...</option>
+                                </select>
+                            </div> 
+                        </div>
+                        <div className = "text-center">
+                            <button type="submit" className="btn btn-primary btn-md">Save</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </>);
