99234982d71e3f52640b6bd0d4064f8b832e9be5
gracedillingham <44414278+gracedillingham@users.noreply.github.com>
2020-11-30 16:57:32 -0600

    Joey's course updates- connected to api

--- frontend/src/app/CourseDashboard.jsx
+++ frontend/src/app/CourseDashboard.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 import { Header } from './Header';
+import {Course} from '../models/Class';
+import {ProductRepository} from '../Api/productRepository';
 
 export class CourseDashboard extends React.Component{
+    
+        constructor(props){
+        super(props);
+            this.state = {
+
+       courses: [],
+       userID: 1,
+     
+
+            };
+    this.productRepo = new ProductRepository();
+
+    }//end state
+
+
+    componentDidMount(){
+        this.productRepo.getCourses(this.state.userID)
+        .then(res => {
+            console.log(res)
+            res.forEach(ele => {
+                this.setState({courses:[...this.state.courses, new Course(ele.classID, ele.classDaysID, ele.description, ele.yearOffered, ele.seasonOffered, ele.classTimeStart, ele.classTimeEnd)]});
+                
+          });
+     
+        console.log(this.state);
+            })
+            .catch(res => console.log(res));
+    }
 
     render() {
         return(<>
@@ -23,32 +53,33 @@ export class CourseDashboard extends React.Component{
                                     <th scope="col">Professor</th>
                                     <th scope="col">Start Time</th>
                                     <th scope="col">End Time</th>
-                                    <th scope="col">Semester Offered</th>
-                                    <th scope="col">Description</th>
-                                    <th scope="col">Edit</th>
+                                    {/* <th scope="col">Professor</th>
+                                        <th scope="col">Description</th>
+                                        <th scope="col">Edit</th> */}
                                 </tr>
                             </thead>
                             <tbody>
+                                { this.state.courses.map((x) => <tr key = {x.classID}> <td>{x.classDaysID}</td> <td>{x.description}</td> <td>{x.yearOffered}</td> <td>{x.seasonOffered}</td><td>{x.classTimeStart}</td> <td>{x.classTimeEnd}</td> </tr>)}
                                 <tr>
-                                    <td>ASIM 1310</td>
+                                    {/* <td>ASIM 1310</td>
                                     <td>Creative Coding 1</td>
                                     <td>MWF</td>
                                     <td>James Burns</td>
                                     <td>10:00 AM</td>
                                     <td>10:50 AM</td>
                                     <td>January 2021</td>
-                                    <td>Explore the mystery of code in a creative way.</td>
+                                    <td>Explore the mystery of code in a creative way.</td> */}
                                     <button type="button" className="btn btn-primary btn-sm rounded" onClick={() => this.props.history.push("/viewCourses")}>View Course</button>
                                 </tr>
                                 <tr>
-                                    <td>ASPT 1300</td>
+                                    {/* <td>ASPT 1300</td>
                                     <td>Introduction to Painting</td>
                                     <td>TuTh</td>
                                     <td>Nyugen Smith</td>
                                     <td>12:30 PM</td>
                                     <td>3:20 PM</td>
                                     <td>Spring 2021</td>
-                                    <td>Learn how to use a paintbrush like a pro</td>
+                                    <td>Learn how to use a paintbrush like a pro</td> */}
                                     <button type="button" className="btn btn-primary btn-sm rounded" onClick={() => this.props.history.push("/viewCourses")}>View Course</button>
                                 </tr>
                             </tbody>
