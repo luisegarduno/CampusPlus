40f2d37659ddd3482799559a20e1393786f3e686
gracedillingham <44414278+gracedillingham@users.noreply.github.com>
2020-12-01 02:04:11 -0600

    Add course model

---
+++ frontend/src/models/Course.js
@@ -0,0 +1,11 @@
+export class Course {
+    constructor(classID, classDaysID, description, yearOffered, seasonOffered, classTimeStart, classTimeEnd) {
+        this.classID = classID;
+        this.classDaysID = classDaysID;
+        this.description = description;
+        this.yearOffered = yearOffered;
+        this.seasonOffered = seasonOffered;
+        this.classTimeStart = classTimeStart;
+        this.classTimeEnd = classTimeEnd;
+    }
+}
